a8246075442189c92f571aa6a5419d13c792e00a
Dominik Danielewicz <d.danielewicz00@gmail.com>
2023-03-24 19:15:45 +0100

    Initial commit
    Project setup and connection to API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import getData from './../api';
+
+class App extends Component {
+  state = {
+    photos: [],
+    isLoading: false,
+    error: null,
+  };
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+
+    try {
+      const photos = getData.fetchImagesWithQuery('cat', 1);
+      console.log(photos);
+      this.setState({ photos });
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CircleLoader from 'react-spinners/ClipLoader';
+
+const Loader = () => {
+  return <CircleLoader color="#36d7b7" />;
+};
+
+export default Loader;
